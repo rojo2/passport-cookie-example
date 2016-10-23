54cd53d56a2fe6bdec96b8f7dd75eddf494776a1
Aitor <aitor@rojo2.com>
2016-10-23 19:57:13 +0200

    Fixes flow

--- index.js
+++ index.js
@@ -7,17 +7,26 @@ const path = require("path");
 const app = express();
 
 const COOKIE_NAME = "token";
+const USER = {
+  userid: "UserId",
+  username: "Username",
+  email: "E-mail"
+};
 
 passport.use(new CookieStrategy({ cookieName: COOKIE_NAME }, function(token, next) {
   if (token === "aVwXd7c") {
-    return next(null, {
-      userid: "UserId",
-      username: "Username",
-      email: "E-mail"
-    });
+    return next(null, USER);
   }
   return next(null, false);
 }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.userid);
+});
+
+passport.deserializeUser(function(id, done) {
+  return done(null, USER);
+});
 
 app.use(cookieParser());
 app.use(passport.initialize());
@@ -30,12 +39,16 @@ app.post("/logout", (req, res) => {
   res.clearCookie(COOKIE_NAME).redirect("/");
 });
 
-app.get("/", (req, res) => {
-  res.sendfile(path.join(__dirname,"index.html"));
+app.get("/", (req, res, next) => {
+  passport.authenticate("cookie", function(err, user) {
+    if (err) { return next(err); }
+    if (!user) { return res.sendFile(path.join(__dirname,"index.html")); }
+    return res.redirect("/profile");
+  })(req,res,next);
 });
 
-app.get("/profile", passport.authenticate("cookie", { session: false }), (req, res) => {
-  res.sendfile(path.join(__dirname,"profile.html"));
+app.get("/profile", passport.authenticate("cookie", { session: false, failureRedirect: "/" }), (req, res) => {
+  res.sendFile(path.join(__dirname,"profile.html"));
 });
 
 app.listen(process.env.PORT || 4000);
